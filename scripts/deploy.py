1571a1c62cdf299e304b1df6d7e243adbbf152d5
Ayush Shukla <shuklaayush247@gmail.com>
2021-09-02 21:37:39 +0530

    fix: remove polygon-specific code

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -100,23 +100,12 @@ def deploy():
   deposit_amount = 50 * 10**18
 
   # Convert ETH -> WETH
-  # interface.IWETH(WETH).deposit({"value": deposit_amount, "from": deployer})
-  ########### NOTE: Poly - Convert MATIC -> WETH
-  WMATIC = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270"
-  router.swapExactETHForTokens(
-      0,
-      [WMATIC, WETH],
-      deployer,
-      9999999999999999,
-      {"from": deployer, "value": deposit_amount}
-  )
-  ##############
+  interface.IWETH(WETH).deposit({"value": deposit_amount, "from": deployer})
 
   # Buy WBTC with path ETH -> WETH -> WBTC
   router.swapExactETHForTokens(
       0,
-      # [WETH, WBTC],
-      [WMATIC, WETH, WBTC], # NOTE: Poly
+      [WETH, WBTC],
       deployer,
       9999999999999999,
       {"value": deposit_amount, "from": deployer}
