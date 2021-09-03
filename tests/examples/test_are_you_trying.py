015c8a9f64ecafb2f4221c47689e36b2d58c3046
Ayush Shukla <shuklaayush247@gmail.com>
2021-09-03 22:41:15 +0530

    test: add custom test to check behaviour

--- tests/examples/test_are_you_trying.py
+++ tests/examples/test_are_you_trying.py
@@ -41,7 +41,10 @@ def test_are_you_trying(deployer, sett, strategy, want):
 
   ## TEST 2: Is the Harvest profitable?
   harvest = strategy.harvest({"from": deployer})
-  event = harvest.events["Harvest"][1]
+  # Find strategy harvest event
+  for event in harvest.events['Harvest']:
+    if len(event.keys()) == 2 and set(event.keys()) == {'harvested', 'blockNumber'}:
+      break
   # If it doesn't print, we don't want it
   assert event["harvested"] > 0
 
--- tests/test_custom.py
+++ tests/test_custom.py
@@ -11,5 +11,57 @@ from helpers.time import days
 """
 
 
-def test_my_custom_test():
-  assert True
+def test_my_custom_test(deployer, vault, sett, controller, strategy, want, settKeeper, strategyKeeper):
+    # Setup
+    snap = SnapshotManager(vault, strategy, controller, "StrategySnapshot")
+    depositAmount = want.balanceOf(deployer)
+
+    # Deposit
+    want.approve(sett, MaxUint256, {"from": deployer})
+    snap.settDeposit(depositAmount, {"from": deployer})
+
+    assert want.balanceOf(sett) > 0
+    assert depositAmount == want.balanceOf(sett)
+    assert strategy.isTendable() == False
+
+    # Earn
+    snap.settEarn({"from": settKeeper})
+    # Earn automatically calls strategy's deposit
+    assert strategy.balanceOfWant() == 0
+    assert strategy.balanceOf() == strategy.balanceOfPool()
+    assert strategy.isTendable() == False
+
+    chain.sleep(days(30))
+    chain.mine()
+
+    # Harvest (twice just to see no errors)
+    assert strategy.checkPendingReward() > 0
+    snap.settHarvest({"from": strategyKeeper})
+    assert strategy.checkPendingReward() == 0
+    assert strategy.balanceOfWant() > 0
+    assert strategy.isTendable() == True
+
+    chain.sleep(3600)
+    chain.mine()
+    snap.settHarvest({"from": strategyKeeper})
+
+    # Tend (twice just to see no errors)
+    snap.settTend({"from": strategyKeeper})
+    assert strategy.isTendable() == False
+    snap.settTend({"from": strategyKeeper})
+
+    chain.sleep(3600)
+    chain.mine()
+
+    # Harvest
+    assert strategy.checkPendingReward() > 0
+    snap.settHarvest({"from": strategyKeeper})
+    assert strategy.checkPendingReward() == 0
+    assert strategy.balanceOfWant() > 0
+    assert strategy.isTendable() == True
+
+    # Withdraw
+    snap.settWithdrawAll({"from": deployer})
+    assert strategy.balanceOf() == strategy.balanceOfPool() == strategy.balanceOfWant() == 0
+    assert strategy.isTendable() == False
+    assert want.balanceOf(deployer) > depositAmount
