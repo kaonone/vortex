c4ed00fce3575377e04bbddcf320a58c3f7cd0bc
joshualyguessennd <joshualyguessennd@icloud.com>
2021-11-11 16:30:51 +0400

    feat: staking mcb

---
+++ interfaces/IMcbStaking.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: AGPL V3.0
+pragma solidity 0.8.4;
+
+
+interface IMcBstaking {
+    function balanceOf(
+        address _account
+    ) external view returns (uint256);
+
+    function calcUnlockTime(
+        address _account,
+        uint256 _amount
+    ) external view returns (uint256);
+
+    function stake(uint256 _amount) external;
+
+    function restake() external;
+
+    function redeem() external;
+}
