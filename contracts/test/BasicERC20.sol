87c61bb4d27b4f8665998a5d660fa39c0c9074c0
joshualyguessennd <75019812+joshualyguessennd@users.noreply.github.com>
2021-12-28 11:07:10 +0000

    Revert "Feat/yearn feedback"

--- contracts/test/BasicERC20.sol
+++ contracts/test/BasicERC20.sol
@@ -1,4 +1,3 @@
-// SPDX-License-Identifier: AGPL V3.0
 pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -9,14 +8,11 @@ ERC20 wrapper used for testing. Any account can call mint()
 
 contract BasicERC20 is ERC20 {
     constructor(string memory name_, string memory symbol_)
+        public
         ERC20(name_, symbol_)
     {}
 
     function mint(uint256 amount) public {
         _mint(msg.sender, amount);
-    }
-
-    function decimals() public view virtual override returns (uint8) {
-        return 6;
     }
 }
