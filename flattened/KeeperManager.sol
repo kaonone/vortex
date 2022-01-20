93662504b4c9bbe649ca6c2545b26280a9c903e2
Genysys <samuel.b.dare@gmail.com>
2022-01-20 12:20:31 +0400

    scratch"

---
+++ flattened/KeeperManager.sol
@@ -0,0 +1,251 @@
+// SPDX-License-Identifier: AGPL V3.0
+
+pragma solidity 0.8.4;
+
+
+
+// Part: IStrategy
+
+interface IStrategy {
+    function withdraw(uint256) external returns (uint256, uint256);
+
+    function harvest() external;
+}
+
+// Part: OpenZeppelin/openzeppelin-contracts@4.0.0/Context
+
+/*
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+        return msg.data;
+    }
+}
+
+// Part: OpenZeppelin/openzeppelin-contracts@4.0.0/Ownable
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor () {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
+
+// Part: OpenZeppelin/openzeppelin-contracts@4.0.0/Pausable
+
+/**
+ * @dev Contract module which allows children to implement an emergency stop
+ * mechanism that can be triggered by an authorized account.
+ *
+ * This module is used through inheritance. It will make available the
+ * modifiers `whenNotPaused` and `whenPaused`, which can be applied to
+ * the functions of your contract. Note that they will not be pausable by
+ * simply including this module, only once the modifiers are put in place.
+ */
+abstract contract Pausable is Context {
+    /**
+     * @dev Emitted when the pause is triggered by `account`.
+     */
+    event Paused(address account);
+
+    /**
+     * @dev Emitted when the pause is lifted by `account`.
+     */
+    event Unpaused(address account);
+
+    bool private _paused;
+
+    /**
+     * @dev Initializes the contract in unpaused state.
+     */
+    constructor () {
+        _paused = false;
+    }
+
+    /**
+     * @dev Returns true if the contract is paused, and false otherwise.
+     */
+    function paused() public view virtual returns (bool) {
+        return _paused;
+    }
+
+    /**
+     * @dev Modifier to make a function callable only when the contract is not paused.
+     *
+     * Requirements:
+     *
+     * - The contract must not be paused.
+     */
+    modifier whenNotPaused() {
+        require(!paused(), "Pausable: paused");
+        _;
+    }
+
+    /**
+     * @dev Modifier to make a function callable only when the contract is paused.
+     *
+     * Requirements:
+     *
+     * - The contract must be paused.
+     */
+    modifier whenPaused() {
+        require(paused(), "Pausable: not paused");
+        _;
+    }
+
+    /**
+     * @dev Triggers stopped state.
+     *
+     * Requirements:
+     *
+     * - The contract must not be paused.
+     */
+    function _pause() internal virtual whenNotPaused {
+        _paused = true;
+        emit Paused(_msgSender());
+    }
+
+    /**
+     * @dev Returns to normal state.
+     *
+     * Requirements:
+     *
+     * - The contract must be paused.
+     */
+    function _unpause() internal virtual whenPaused {
+        _paused = false;
+        emit Unpaused(_msgSender());
+    }
+}
+
+// File: KeeperManager.sol
+
+contract KeeperManager is Ownable, Pausable {
+    address public strategy;
+    address public registryContract;
+    uint256 public cooldown;
+    uint256 public lastTimestamp;
+
+    event CooldownSet(uint256 cooldown);
+    event StrategySet(address indexed strategy);
+    event RegistryContractSet(address indexed registryContract);
+
+    constructor(
+        address _strategy,
+        uint256 _cooldown,
+        address _registryContract
+    ) {
+        strategy = _strategy;
+        cooldown = _cooldown;
+        registryContract = _registryContract;
+    }
+
+    function setStrategy(address _strategy) public onlyOwner {
+        strategy = _strategy;
+        emit StrategySet(_strategy);
+    }
+
+    function setCoolDown(uint256 _cooldown) public onlyOwner {
+        cooldown = _cooldown;
+        emit CooldownSet(_cooldown);
+    }
+
+    function setRegistryContract(address _registryContract) public onlyOwner {
+        registryContract = _registryContract;
+        emit RegistryContractSet(_registryContract);
+    }
+
+    function checkUpkeep(
+        bytes calldata /* checkData */
+    )
+        external
+        returns (
+            bool upkeepNeeded,
+            bytes memory /* performData */
+        )
+    {
+        upkeepNeeded = (block.timestamp - lastTimestamp) > cooldown;
+    }
+
+    function performUpkeep(
+        bytes calldata /* performData */
+    ) external {
+        require(msg.sender == registryContract, "!keeper");
+        require(
+            (block.timestamp - lastTimestamp) > cooldown,
+            "harvest not needed"
+        );
+        lastTimestamp = block.timestamp;
+        IStrategy(strategy).harvest();
+    }
+}
