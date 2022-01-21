895dc8cafc9f47f728ec67f47e2d554d10ef808f
joshualyguessennd <joshualyguessennd@icloud.com>
2022-01-21 22:05:15 +0400

    forge install: openzepplin-contracts

--- contracts/utils/UtilProxyAdmin.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity 0.8.4;
-pragma experimental ABIEncoderV2;
-
-import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
-
-contract UtilProxyAdmin is ProxyAdmin {}
--- interfaces/IStrategy.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL V3.0
-pragma solidity 0.8.4;
-
-interface IStrategy {
-    function withdraw(uint256) external returns (uint256, uint256);
-
-    function harvest() external;
-}
