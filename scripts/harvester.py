e3d873db4ef66650407345d9302f6a8d9c8d8eb0
Genysys <samuel.b.dare@gmail.com>
2022-02-16 20:39:30 +0400

    chore: update strategy address

--- scripts/harvester.py
+++ scripts/harvester.py
@@ -12,9 +12,11 @@ def main():
     constant = scripts.constants
     load_dotenv(find_dotenv())
     admin_key = os.getenv("DEPLOYER_PRIVATE_KEY")
-    strategy = interface.IStrategy(constant.STRATEGY_ADDRESS)
+    strategy_1 = interface.IStrategy(constant.STRATEGY_ADDRESS_1)
+    strategy_2 = interface.IStrategy(constant.STRATEGY_ADDRESS_2)
     harvester = accounts.add(admin_key)
     # initialize strategy
-    strategy.harvest({"from": harvester})
+    strategy_1.harvest({"from": harvester})
+    strategy_2.harvest({"from": harvester})
     time.sleep(2)
     print("strategy harvested")
