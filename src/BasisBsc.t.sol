dbd9adca95cd5a20c3ca95ea030562ef2ca22bb9
joshualyguessennd <joshualyguessennd@icloud.com>
2022-02-16 23:28:42 +0400

    feat: test_forge_init

--- src/BasisBsc.t.sol
+++ src/BasisBsc.t.sol
@@ -57,7 +57,7 @@ contract BasisTestBsc is DSTest {
     function setUp() public {
         vault = new BasisVault();
         vm.startPrank(deployer);
-        vault.initialize(_want, _depositLimit, _individualDepositLimit);
+        vault.initialize(_want, _depositLimit, _individualDepositLimit, 0, 2500);
         strategy = new BasisStrategy();
         strategy.initialize(
             _longAsset,
