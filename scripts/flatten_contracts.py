f539f7a129b823b0dfe437180164564c2c62a059
Dmitriy Serdtsev <in19farkt@gmail.com>
2022-03-08 16:08:16 +0300

    ci: fix Solidity Security job

--- scripts/flatten_contracts.py
+++ scripts/flatten_contracts.py
@@ -1,3 +1,4 @@
+# Working with eth-brownie<1.17.0
 import sys
 from os import path
 from brownie import *
@@ -5,12 +6,12 @@ from brownie import *
 
 def _flattener(contracts_to_flatten):
     for contract_obj in contracts_to_flatten:
-        contract_obj.get_verification_info()
+        contract_info = contract_obj.get_verification_info()
         flatten_file_name = path.join(
             "flattened", ".".join([contract_obj._name, "sol"])
         )
         with open(flatten_file_name, "w") as fl_file:
-            fl_file.write(contract_obj._flattener.flattened_source)
+            fl_file.write(contract_info["flattened_source"])
 
 
 def main():
